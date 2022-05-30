5011c57b41a8d1e254e7f6f62848619b650c8fd4
Cucumber <cucumber.alex93@gmail.com>
2022-05-30 11:05:24 +0300

    Add fail

--- home_1.1.py
+++ home_1.1.py
@@ -1,11 +1,17 @@
-man_name =input('Привет, как тебя зовут')
-print(man_name,'Сейчас мы будем печь куличи')
-egg =int(input('Сколько у тебя есть яиц ?'))
-milk =egg*83
-flour =egg*166
-butter =egg*33
-sugar =egg*50
-raisin =egg*46
-yeast =egg*2
-print('Тебе понадобиться на это количество яиц''\nмолоко- мл',milk,'\nмука -гр',flour,'\nмасло сливочное -гр',butter,)
-print('сахар -гр',sugar,'\nизюм -гр',raisin,'\nдрожжи сухие -гр',yeast,'\nПриятного аппетита')
+MILK = 500/6
+FLOUR = 1000/6
+BUTTER = 200/6
+SUGAR = 300/6
+RAISIN = 275/6
+YEAST = 12/6
+man_name = input('Привет, как тебя зовут')
+print(man_name, 'Сейчас мы будем печь куличи')
+egg = int(input('Сколько у тебя есть яиц ?'))
+milk = egg*MILK
+flour = egg*FLOUR
+butter = egg*BUTTER
+sugar = egg*SUGAR
+raisin = egg*RAISIN
+yeast = egg*YEAST
+print('Тебе понадобиться на это количество яиц''\nмолоко- мл', milk, '\nмука -гр', flour, '\nмасло сливочное -гр', butter, )
+print('сахар -гр', sugar, '\nизюм -гр', raisin, '\nдрожжи сухие -гр', yeast, '\nПриятного аппетита')
