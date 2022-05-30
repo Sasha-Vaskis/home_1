5bc60580c976a1b5acd36406dfafc7ed735f134b
Cucumber <cucumber.alex93@gmail.com>
2022-05-30 08:04:37 +0300

    Add fail

---
+++ home_1.1.py
@@ -0,0 +1,11 @@
+man_name =input('Привет, как тебя зовут')
+print(man_name,'Сейчас мы будем печь куличи')
+egg =int(input('Сколько у тебя есть яиц ?'))
+milk =egg*83
+flour =egg*166
+butter =egg*33
+sugar =egg*50
+raisin =egg*46
+yeast =egg*2
+print('Тебе понадобиться на это количество яиц''\nмолоко- мл',milk,'\nмука -гр',flour,'\nмасло сливочное -гр',butter,)
+print('сахар -гр',sugar,'\nизюм -гр',raisin,'\nдрожжи сухие -гр',yeast,'\nПриятного аппетита')
